92d75d59a9690c6c158b9b06b76d75fc11ce6958
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-06-12 15:46:53 +0200

    Define traits

--- ev-cli/src/cmd/command_handlers.rs
+++ ev-cli/src/cmd/command_handlers.rs
@@ -3,13 +3,14 @@ pub trait TerminalHandler: AddCommand + RemoveCommand + ListCommand {
 }
 
 pub trait AddCommand {
-    
+    fn add_variable(&self, var_name: String, value: String) -> ();
 }
 
 pub trait ListCommand {
-    
+    fn list_variables(&self) -> [String];
+    fn list_terminals(&self) -> [String];
 }
 
 pub trait RemoveCommand {
-    
+    fn remove_variable(&self, var_name: String) -> ();
 }
