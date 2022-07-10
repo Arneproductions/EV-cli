859cd2dfb000541e54edd968848d3981abbace6c
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-07-10 22:33:38 +0200

    Create ctor function

--- ev-cli/src/terminals/zsh_handler.rs
+++ ev-cli/src/terminals/zsh_handler.rs
@@ -1,23 +1,46 @@
+use std::{fs};
+
 use crate::cmd::command_handlers::{RemoveCommand, AddCommand, ListCommand};
 
 use super::TerminalHandler;
 
+const USER_PATH: String = String::from("/sdf");
+const GLOBAL_PATH: String = String::from("bla bla");
+
 pub struct ZSHHandler {
 
-    // TODO: These fields should not be public. This was only for learning purposes
-    pub user_path: String,
-    pub gloabl_path: String,
+    user_path: String,
+    global_path: String,
+}
+
+impl ZSHHandler {
+    pub fn new() -> Self {
+        Self { 
+            user_path: USER_PATH, 
+            global_path: GLOBAL_PATH 
+        }
+    }
+
+    fn read_file(&self, path: &String) -> String {
+        let content = fs::read_to_string(path)
+            .expect("Could not read the environment configuration file!");
+
+        return content;
+    }
 }
 
 impl RemoveCommand for ZSHHandler {
     fn remove_variable(&self, var_name: String) -> () {
+
+        let content = self.read_file(&self.user_path);
         return;
     }
 }
 
 impl AddCommand for ZSHHandler {
     fn add_variable(&self, var_name: String, value: String) -> () {
-        // std::File::open("blabla.txt");
+        
+        let content = self.read_file(&self.user_path);
 
         return;
     }
@@ -25,10 +48,16 @@ impl AddCommand for ZSHHandler {
 
 impl ListCommand for ZSHHandler {
     fn list_variables(&self) -> Vec<String> {
+
+        let content = self.read_file(&self.user_path);
+        
         return Vec::new();
     }
 
     fn list_terminals(&self) -> Vec<String> {
+
+        let content = self.read_file(&self.user_path);
+
         return Vec::new();
     }
 }
