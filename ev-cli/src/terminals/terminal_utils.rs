76e37819367cffff6a42111a8b72ee97f20480fe
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-09-27 22:44:34 +0200

    Add a way to get terminals

--- ev-cli/src/terminals/terminal_utils.rs
+++ ev-cli/src/terminals/terminal_utils.rs
@@ -1,9 +1,23 @@
-use std::{fs, path};
+use std::{fs};
 
 /// Searches for terminal configurations and lists the available terminals
 pub fn get_terminals() -> Vec<String> {
 
-    let path = path::Path::new("");
-    fs::read_dir(path);
-    return Vec::new();
+    let mut terminals = Vec::new();
+    let terminal_names = ["zsh", "bash"];
+    let dir = fs::read_dir("~").unwrap();
+    
+    for path in dir {
+        
+        let p = path.unwrap();
+        let file = p.file_name().into_string().unwrap();
+
+        for name in terminal_names {
+            if file.contains(name) {
+                terminals.push(name.to_owned());
+            }
+        }
+        
+    }
+    return terminals;
 }
