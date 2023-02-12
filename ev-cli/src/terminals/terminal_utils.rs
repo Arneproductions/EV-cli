02c9460c708526916aaae0d06fd929450f59e77b
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2023-02-12 11:12:46 +0100

    Fix home path
    * Add linux and windows compatible home paths
    
    * Convert the Hashset to a vector
    
    * Add dependencies for the home path and change representation visualy

--- ev-cli/src/terminals/terminal_utils.rs
+++ ev-cli/src/terminals/terminal_utils.rs
@@ -1,23 +1,33 @@
-use std::{fs};
+use std::{fs, path::Path, collections::HashSet};
+use home;
 
 /// Searches for terminal configurations and lists the available terminals
-pub fn get_terminals() -> Vec<String> {
+pub fn get_terminals() -> HashSet<String> {
 
-    let mut terminals = Vec::new();
-    let terminal_names = ["zsh", "bash"];
-    let dir = fs::read_dir("~").unwrap();
+    match home::home_dir() {
+        Some(path) => return search_terminals(path.as_path()), 
+        None => return HashSet::new()    
+    }
+}
+
+fn search_terminals(path: &Path) -> HashSet<String> {
     
-    for path in dir {
+    let mut terminals = HashSet::new();
+    let terminal_names = ["zsh", "bash"];
+    let dir = fs::read_dir(path).unwrap();
+
+    for file_path in dir {
         
-        let p = path.unwrap();
+        let p = file_path.unwrap();
         let file = p.file_name().into_string().unwrap();
 
         for name in terminal_names {
             if file.contains(name) {
-                terminals.push(name.to_owned());
+                terminals.insert(name.to_owned());
             }
         }
         
     }
+
     return terminals;
 }
