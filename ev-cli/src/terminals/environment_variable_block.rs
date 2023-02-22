8287eca0a89555d19186bfaa8f1cc49f8e587dbd
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2023-02-22 18:48:21 +0100

    Add test and fix to_string such that it adds misc lines

--- ev-cli/src/terminals/environment_variable_block.rs
+++ ev-cli/src/terminals/environment_variable_block.rs
@@ -31,7 +31,7 @@ impl EnvironmentVariableBlock {
     }
 
     pub fn to_string(&self) -> String {
-
+        // TODO: Fix so that the misc lines is being added as well
         let mut value = String::new();
 
         for line in self.misc.iter() {
@@ -40,6 +40,26 @@ impl EnvironmentVariableBlock {
 
         let ev_line = format!("export {}={}", self.key, self.value);
 
-        return ev_line;
+        return format!("{}{}\n", value, ev_line);
     }
+}
+
+#[cfg(test)]
+mod environment_variable_block_tests {
+    use super::EnvironmentVariableBlock;
+
+
+    #[test]
+    fn to_string_returns_misc_lines_and_ev_line() {
+
+        let mut ev_block = EnvironmentVariableBlock::new();
+
+        ev_block.add_misc("#This is a comment\n");
+        ev_block.add_misc("#This is another comment that we also need to take care of\n");
+        ev_block.set_environment_variable("key", "value");
+
+        let result = ev_block.to_string();
+        assert_eq!(result, "#This is a comment\n#This is another comment that we also need to take care of\nexport key=value\n")
+    }
+
 }
